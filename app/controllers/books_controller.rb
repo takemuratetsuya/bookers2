1dcf407dfface8820b7745ce5d2cfdce6ce40b84
takemuratetsuya <109710030+takemuratetsuya@users.noreply.github.com>
2022-09-20 09:27:40 +0000

    [Add]ActiveStorage.FontAwesome導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,20 @@ class BooksController < ApplicationController
     @book = Book.new
     @user = current_user
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @booknew = @book
+      @user = current_user
+      render :index
+    end
+  end
 
   def show
     @booknew = Book.new
@@ -14,6 +28,30 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     unless @book.user == current_user
+      redirect_to books_path
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    else
+      if @book.update(book_params)
+        flash[:notice] = "You have updated book successfully."
+        redirect_to book_path(@book.id)
+      else
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    else
+      @book.destroy
       redirect_to books_path
     end
   end
