0bebb0ce9b01e9f6e452001b4ed5a231273fc3f4
takemuratetsuya <109710030+takemuratetsuya@users.noreply.github.com>
2022-11-14 03:10:18 +0000

    [Add]application 記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,20 @@ class UsersController < ApplicationController
     end
   end
   
+  def update
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    else
+      if @user.update(user_params)
+        flash[:notice] = "You have updated user successfully."
+        redirect_to user_path(@user.id)
+      else
+        render :edit
+      end
+    end
+  end
+  
   private
 
 def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  #get 'users/index'
+  #get 'users/show'
+  #get 'users/edit'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
+  #get 'homes/top'
+  
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about"
+  resources :books, only:[:index, :show, :edit, :create, :update, :destroy]
+  resources :users, only:[:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
